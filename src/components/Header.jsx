57cbd5dabcf07696d3edd9cb6d375a3a5e2dda5b
Prajwal Joshi <prajwaljoshi@Prajwals-MacBook-Pro.local>
2023-05-23 22:12:03 +0545

    day1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
-import { HStack, Heading } from '@chakra-ui/react'
+import { HStack, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Text } from '@chakra-ui/react'
 
 
 const Header = () => {
   return (
-    <HStack w={'full'} bgColor={'gray.300'}>
-      <Heading>Header</Heading>
-    </HStack>  )
+        <HStack w={'full'} bgColor={'gray.300'}>
+            <Text color={'green'}>upwork</Text>
+            <Breadcrumb>
+                <BreadcrumbItem>
+                    <BreadcrumbLink href='#'>Home</BreadcrumbLink>
+                </BreadcrumbItem>
+
+                <BreadcrumbItem>
+                    <BreadcrumbLink href='#'>Docs</BreadcrumbLink>
+                </BreadcrumbItem>
+
+                <BreadcrumbItem isCurrentPage>
+                    <BreadcrumbLink href='#'>Breadcrumb</BreadcrumbLink>
+                </BreadcrumbItem>
+                </Breadcrumb>
+        </HStack>  
+    )
 }
 
 export default Header
