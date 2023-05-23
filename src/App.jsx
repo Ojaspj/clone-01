1e7aa15670e0e59de023c93962e055a132abc9a0
Prajwal Joshi <prajwaljoshi@Prajwals-MacBook-Pro.local>
2023-05-23 21:30:08 +0545

    initial

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        HEllo
+      </div>
+     
+    </>
+  )
+}
+
+export default App
